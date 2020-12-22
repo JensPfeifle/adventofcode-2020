c26642dc249118a5f2888c6e59d2c2eb12c4dd69
Jens Pfeifle <jens@pfeifle.tech>
2020-12-22 17:04:03 +0100

    Day 18 - Part 1 (python)

--- day18/18.py
+++ day18/18.py
@@ -1,60 +1,80 @@
 import operator
 
-import pyparsing
+import pytest
 from pyparsing import (
     Forward,
     Group,
     Literal,
+    ParseResults,
     Suppress,
     Word,
     ZeroOrMore,
     nums,
-    ParseResults,
 )
 
-example1 = "1 + 2 * 3 + 4 * 5 + 6"
-example2 = "1 + (2 * 3) + (4 * (5 + 6))"
-example3 = "2 * 3 + (4 * 5)"
 # map operator symbols to corresponding arithmetic operations
-opn = {
+OPS = {
     "+": operator.add,
     "*": operator.mul,
 }
 
 
-result = None
+def parse(string):
+    plus = Literal("+")
+    mult = Literal("*")
 
-stack = []
+    oper = plus ^ mult
+
+    lpar, rpar = map(Suppress, "()")
+    number = Word(nums)
+    expr = Forward()
+    group = Group(lpar + expr + rpar)
+    atom = oper + (expr ^ group)
+    expr <<= (number ^ group) + ZeroOrMore(atom)
+
+    return expr.parseString(string)
 
 
 def evaluate(parsed: ParseResults) -> int:
-    if not stack:
-        stack.append(parsed[0])  # value
-        stack.append(parsed[1])  # operator
+    left, op, right = parsed[0:3]
 
-    operation = stack.pop()
-    left = stack.pop()
+    if not isinstance(left, int):
+        left = int(left) if isinstance(left, str) else evaluate(left)
+    if not isinstance(right, int):
+        right = int(right) if isinstance(right, str) else evaluate(right)
 
-    op = opn[operation]
-    right = parsed[2:]
-    if len(right) == 1:
+    evaluated = OPS[op](int(left), int(right))
+    if len(parsed) > 3:
+        return evaluate([evaluated] + parsed[3:])
+    return evaluated
 
-    print(f"evalutating {left} {operation} {right}")
+
+# run with pytest 18.py
+@pytest.mark.parametrize(
+    "expression, expected",
+    [
+        ("1 + 2", 3),
+        ("1 + 2 * 3 + 4 * 5 + 6", 71),
+        ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+        ("2 * 3 + (4 * 5)", 26),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+    ],
+)
+def test_parse_and_evaluate(expression, expected):
+    parsed = parse(expression)
+    assert evaluate(parsed) == expected
 
 
-plus = Literal("+")
-mult = Literal("*")
+if __name__ == "__main__":
+    with open("18.in") as f:
+        lines = f.read().strip().split("\n")
 
-oper = plus ^ mult
-
-lpar, rpar = map(Suppress, "()")
-number = Word(nums)
-
-expr = Forward()
-group = Group(lpar + expr + rpar)
-atom = oper + (expr ^ group)
-expr <<= number + ZeroOrMore(atom)
-
-ex = example2
-print(ex)
-print(evaluate(expr.parseString(ex)))
+    print("part1", end="")
+    total = 0
+    for line in lines:
+        v = evaluate(parse(line))
+        print(".", end="", flush=True)
+        total += v
+    print(f"{total}")
