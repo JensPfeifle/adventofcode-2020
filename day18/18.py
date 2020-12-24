1201eeeb8fd5031b6a5f94d2e0f5cd0733095ddd
Jens Pfeifle <jens@pfeifle.tech>
2020-12-24 11:13:51 +0100

    [WIP] Day 18 - part2

--- day18/18.py
+++ day18/18.py
@@ -49,6 +49,18 @@ def evaluate(parsed: ParseResults) -> int:
         return evaluate([evaluated] + parsed[3:])
     return evaluated
 
+def evaluate_part2(parsed: ParseResults) -> int:
+    left, op, right = parsed[0:3]
+
+    if not isinstance(left, int):
+        left = int(left) if isinstance(left, str) else evaluate(left)
+    if not isinstance(right, int):
+        right = int(right) if isinstance(right, str) else evaluate(right)
+
+    evaluated = OPS[op](int(left), int(right))
+    if len(parsed) > 3:
+        return evaluate([evaluated] + parsed[3:])
+    return evaluated
 
 # run with pytest 18.py
 @pytest.mark.parametrize(
@@ -63,9 +75,25 @@ def evaluate(parsed: ParseResults) -> int:
         ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
     ],
 )
-def test_parse_and_evaluate(expression, expected):
+def test_parse_and_evaluate_part1(expression, expected):
     parsed = parse(expression)
     assert evaluate(parsed) == expected
+
+
+@pytest.mark.parametrize(
+    "expression, expected",
+    [
+        ("1 + 2 * 3 + 4 * 5 + 6", 231),
+        ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+        ("2 * 3 + (4 * 5)", 46),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340),
+    ],
+)
+def test_parse_and_evaluate_part2(expression, expected):
+    parsed = parse(expression)
+    assert evaluate_part2(parsed) == expected
 
 
 if __name__ == "__main__":
